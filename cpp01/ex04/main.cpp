5e1564d5ca2ba13b6e70accfc8f2890ecf49b704
Liam Cohen <licohen@f2r6s3.clusters.42paris.fr>
2025-06-12 18:00:18 +0200

    good

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: licohen <licohen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 17:32:02 by licohen           #+#    #+#             */
-/*   Updated: 2025/06/11 16:48:10 by licohen          ###   ########.fr       */
+/*   Updated: 2025/06/12 15:11:22 by licohen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,25 @@ int main(int argc, char* argv[])
 {
 	if (argc != 4)
 	{
-		std::cerr << "Usage: " << argv[0] << " <filename> <string_to_replace> <replacement_string>" << std::endl;
+		std::cerr << "Usage: " << argv[0] << " <filename> <s1> <s2>" << std::endl;
 		return 1;
 	}
 
 	const std::string filename = argv[1];
-	const std::string from = argv[2];
-	const std::string to = argv[3];
+	const std::string s1 = argv[2];
+	const std::string s2 = argv[3];
 
-	if (from.empty()) {
-		std::cerr << "Error: The string to replace (from) cannot be empty." << std::endl;
+	if (s1.empty()) 
+	{
+		std::cerr << "Error: The string s2 replace (s1) cannot be empty." << std::endl;
 		return 1;
 	}
 
-	if (!Replace::process(filename, from, to))
+	if (!Replace::process(filename, s1, s2))
 	{
 		std::cerr << "Error: Failed to process file." << std::endl;
 		return 1;
 	}
-	std::cout << "Replacement completed. Output saved to " << filename << ".replace" << std::endl;
+	std::cout << "Output saved to " << filename << ".replace" << std::endl;
 	return 0;
 }
--- cpp01/ex04/utils.cpp
+++ cpp01/ex04/utils.cpp
@@ -6,40 +6,31 @@
 /*   By: licohen <licohen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 21:10:45 by licohen           #+#    #+#             */
-/*   Updated: 2025/06/11 17:01:00 by licohen          ###   ########.fr       */
+/*   Updated: 2025/06/12 13:40:49 by licohen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils.hpp"
 
-std::string Replace::replaceAll(const std::string& content, const std::string& from, const std::string& to)
+std::string Replace::replaceAll(const std::string& content, const std::string& s1, const std::string& s2)
 {
-    if (from.empty())
+    if (s1.empty())
         return content;
-    
     std::string result;
     size_t pos = 0;
     size_t prev = 0;
-
-    while ((pos = content.find(from, prev)) != std::string::npos)
+    while ((pos = content.find(s1, prev)) != std::string::npos)
     {
-        // Ajouter la partie avant l'occurrence trouvée
         result += content.substr(prev, pos - prev);
-        
-        // Ajouter la chaîne de remplacement
-        result += to;
-        
-        // Avancer après la chaîne trouvée
-        prev = pos + from.length();
+        result += s2;
+        prev = pos + s1.length();
     }
-
-    // Ajouter la dernière partie après la dernière occurrence
     result += content.substr(prev);
     
     return result;
 }
 
-bool Replace::process(const std::string& filename, const std::string& from, const std::string& to)
+bool Replace::process(const std::string& filename, const std::string& s1, const std::string& s2)
 {
 	std::ifstream inputFile(filename.c_str());
 	if (!inputFile)
@@ -47,16 +38,14 @@ bool Replace::process(const std::string& filename, const std::string& from, cons
 		std::cerr << "Error: Unable to open file: " << filename << std::endl;
 		return false;
 	}
-
 	std::ofstream outputFile((filename + ".replace").c_str());
 	if (!outputFile)
 	{
 		std::cerr << "Error: Unable to create output file: " << filename << std::endl;
 		return false;
 	}
-
 	std::string line;
 	while (std::getline(inputFile, line))
-		outputFile << replaceAll(line, from, to) << std::endl;
+		outputFile << replaceAll(line, s1, s2) << std::endl;
 	return true;
 }
